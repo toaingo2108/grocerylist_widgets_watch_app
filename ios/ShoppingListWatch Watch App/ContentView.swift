a41a6912edfd0f25978a8a51cb51c23e117880e2
Toai Quoc Ngo <toaingoquoc00@gmail.com>
2024-01-24 18:39:25 -0400

    Watch configuration

---
+++ ios/ShoppingListWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoppingListWatch Watch App
+//
+//  Created by Mobile World on 1/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/ShoppingListWatch Watch App/ShoppingListWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListWatchApp.swift
+//  ShoppingListWatch Watch App
+//
+//  Created by Mobile World on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
