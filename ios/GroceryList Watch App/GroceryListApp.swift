c1f89ac566ca266b8a6a51b49b01314f196da1a9
Toai Quoc Ngo <toaingoquoc00@gmail.com>
2024-01-25 10:40:23 -0400

    watch configuration

---
+++ ios/GroceryList Watch App/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList Watch App
+//
+//  Created by Mobile World on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryList_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
